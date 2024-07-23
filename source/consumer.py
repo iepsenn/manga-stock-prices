50b5c96df904b9051f3663176c60583a3efd5df4
iepsenn <lukasiepsen@gmail.com>
2024-07-23 12:48:50 -0300

    build: change docker-compose.yml to use env_file in all services

--- source/consumer.py
+++ source/consumer.py
@@ -3,7 +3,7 @@ from kafka import KafkaConsumer
 # To consume latest messages and auto-commit offsets
 consumer = KafkaConsumer('price',
                          client_id='consumer',
-                         bootstrap_servers=['Kafka00Service:9092'])
+                         bootstrap_servers=['kafka_broker:9092'])
 for message in consumer:
     # message value and key are raw bytes -- decode if necessary!
     # e.g., for unicode: `message.value.decode('utf-8')`
